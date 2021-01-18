b3d2433f4fd8c885b6c94e8d16c2eb554d00db62
Fredrik Wallander <f.m.wallander@gmail.com>
2021-01-18 15:45:43 +0100

    adds some rune stuff

---
+++ Assets/Scripts/RuneData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuneData : MonoBehaviour
+{
+    private string runeName;
+    private string rarity;
+    private Color typeColor;
+    private int amount;
+    private int idNumber;
+
+    public RuneData(string runeName, string rarity, Color color, int amount, int idNumber){
+        this.runeName = runeName;
+        this.rarity = rarity;
+        this.typeColor = color;
+        this.amount = amount;
+        this.idNumber = idNumber;
+    }
+}
---
+++ Assets/Scripts/ThisRune.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThisRune : MonoBehaviour
+{
+    public List<RuneData> thisRune = new List<RuneData>();
+    
+    private string runeName;
+    private string rarity;
+    private Color typeColor;
+    private int amount;
+    private int idNumber;
+
+    private void Start(){
+        throw new NotImplementedException();
+    }
+
+    private void Update(){
+        throw new NotImplementedException();
+    }
+}
