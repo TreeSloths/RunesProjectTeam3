340be39bd9f15a192e5188ccbaac295ee0e6340f
EmilHuzell <65963728+EmilHuzell@users.noreply.github.com>
2021-01-19 11:30:37 +0100

    experiments with runedata constructor

--- Assets/Scripts/RuneData.cs
+++ Assets/Scripts/RuneData.cs
@@ -8,13 +8,15 @@ public class RuneData : MonoBehaviour
     private string rarity;
     private Color typeColor;
     private int amount;
-    private int idNumber;
+    //private int idNumber;
+    private string id;
 
-    public RuneData(string runeName, string rarity, Color color, int amount, int idNumber){
+    public RuneData(string runeName, string rarity, Color color, int amount/*, int idNumber*/){
         this.runeName = runeName;
         this.rarity = rarity;
         this.typeColor = color;
         this.amount = amount;
         this.idNumber = idNumber;
+        this.id = $"{runeName} {rarity} {color} {amount}";
     }
 }
