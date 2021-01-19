35fc95afb63db8a06731977c097353eda989f4dc
EmilHuzell <65963728+EmilHuzell@users.noreply.github.com>
2021-01-19 11:30:45 +0100

    Revert "experiments with runedata constructor"
    This reverts commit 340be39bd9f15a192e5188ccbaac295ee0e6340f.

--- Assets/Scripts/RuneData.cs
+++ Assets/Scripts/RuneData.cs
@@ -8,15 +8,13 @@ public class RuneData : MonoBehaviour
     private string rarity;
     private Color typeColor;
     private int amount;
-    //private int idNumber;
-    private string id;
+    private int idNumber;
 
-    public RuneData(string runeName, string rarity, Color color, int amount/*, int idNumber*/){
+    public RuneData(string runeName, string rarity, Color color, int amount, int idNumber){
         this.runeName = runeName;
         this.rarity = rarity;
         this.typeColor = color;
         this.amount = amount;
         this.idNumber = idNumber;
-        this.id = $"{runeName} {rarity} {color} {amount}";
     }
 }
