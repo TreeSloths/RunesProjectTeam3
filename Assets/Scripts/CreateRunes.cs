fa76f461bec13020af8f5408554509b60e6beb42
Fredrik Wallander <f.m.wallander@gmail.com>
2021-01-19 11:40:07 +0100

    Small changes to scripts

--- Assets/Scripts/CreateRunes.cs
+++ Assets/Scripts/CreateRunes.cs
@@ -4,10 +4,27 @@ public class CreateRunes : MonoBehaviour{
     public RuneTypeList runeTypeList;
     public RarityTypesList rarityTypesList;
     public RuneInventory runeInventory;
-    public int amountOfRunes;
-    private void Start(){
+    public int amountOfRunes = 4;
+    private void CreateNewRune(){
         for (int i = 0; i < amountOfRunes; i++){
-            runeInventory.AddRune(new Rune(runeTypeList.RandomRuneType(), rarityTypesList.RandomRarity()));
+            runeInventory.AddRune(new Rune(runeTypeList.RandomRuneType(), new Rarity("Common")));
+        }
+    }
+    // Merging runes needs to be same color/type
+    // Needs to take the list of added runes in the merge slots.
+    // Need a ref to merge slots?
+    // dropped rune in one slot adds to list
+    // if rune is picked up from slot then remove rune from list
+    public void MergeRunes(Rune[] runes, int amount){
+        
+        if (amount == 2){
+            // 20 % chance for upgraded rarity
+        }
+        if (amount == 3){
+            // 55 % chance for upgraded rarity
+        }
+        if (amount == 4){
+            // 95 % chance for upgraded rarity
         }
     }
 }
--- Assets/Scripts/RuneInventory.cs
+++ Assets/Scripts/RuneInventory.cs
@@ -2,9 +2,9 @@ using System.Collections.Generic;
 
 public class RuneInventory{
     public event System.Action<Rune> RuneAdded;
-    List<Rune> runes;
+    List<Rune> ownedRunes;
     public void AddRune(Rune rune){
-        this.runes.Add(rune);
+        this.ownedRunes.Add(rune);
         this.RuneAdded?.Invoke(rune);
     }
 }
