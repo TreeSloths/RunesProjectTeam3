f5ebd0aca10461ada873ab24d7d90da84c91d039
Victorpetts <vike.pet@hotmail.com>
2021-01-20 10:38:59 +0100

    Adds drag n drop

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler {
+
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    public Transform parentToReturnTo = null;
+    
+    private void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        Debug.Log("OnBeginDrag");
+
+        parentToReturnTo = rectTransform.parent;
+        rectTransform.SetParent(rectTransform.parent.parent);
+        
+        canvasGroup.blocksRaycasts = false;
+    }
+    
+    public void OnDrag(PointerEventData eventData) {
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+    
+    public void OnEndDrag(PointerEventData eventData) {
+        Debug.Log("OnEndDrag");
+        rectTransform.SetParent(parentToReturnTo);
+        
+        canvasGroup.blocksRaycasts = true;
+    }
+
+}
---
+++ Assets/Scripts/DropSlot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropSlot : MonoBehaviour, IDropHandler {
+
+    public int amountOfSpaces;
+
+    public void OnDrop(PointerEventData eventData) {
+        Debug.Log("OnDrop");
+        if (eventData.pointerDrag != null && transform.childCount < amountOfSpaces) {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition =
+                GetComponent<RectTransform>().anchoredPosition;
+            eventData.pointerDrag.GetComponent<Draggable>().parentToReturnTo = transform;
+        }
+    }
+}
