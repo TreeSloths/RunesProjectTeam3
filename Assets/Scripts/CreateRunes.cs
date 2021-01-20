6418c4465e5241e7f0087f1a97ccf024021b087c
Eric Sjöberg <eric@laurila.se>
2021-01-20 15:19:00 +0100

    adds merg stuff

--- Assets/Scripts/CreateRunes.cs
+++ Assets/Scripts/CreateRunes.cs
@@ -1,14 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CreateRunes : MonoBehaviour{
     public RuneInventory runeInventory;
     public int amountOfRunesToCreate;
-    
+
+    public GameObject mergePanel;
     private bool sameType;
-    private GameObject firstRune;
-    private GameObject secondRune;
-    private GameObject thirdRune;
-    private GameObject forthRune;
+    public GameObject[] merginRunes;
+
     private void CreateNewRune(RuneType runeType, Rarity rarity){
         runeInventory.AddRune(new Rune(runeType, rarity,1));
     }
@@ -17,41 +17,34 @@ public class CreateRunes : MonoBehaviour{
             runeInventory.AddRune(new Rune(RuneTypeList.RandomRuneType(), new Rarity("Common"),1));
         }
     }
-    // Merging runes needs to be same color/type
-    // Needs to take the list of added runes in the merge slots.
-    // Need a ref to merge slots?
-    // dropped rune in one slot adds to list
-    // if rune is picked up from slot then remove rune from list
-    public void MergeRunes(Rune[] runes, int amount) {
+    
+    public void MergeRunes() { 
         var chance = Random.Range(1, 100);
-
-        if (amount == 2 && !TypeCheck()) {
-            if (chance > 80) {
-                //Invoke event
-                Debug.Log("Got an upgraded card with 2 cards");
-            }
+        var amount = mergePanel.transform.childCount;
+        
+        if (amount == 2 && chance > 80) {
+            //Invoke event
+            Debug.Log("Got an upgraded card with 2 cards");
             // 20 % chance for upgraded rarity
         }
-        if (amount == 3 && !TypeCheck()) {
-            if (chance > 45) {
-                //Invoke event
-                Debug.Log("Got an upgraded card upgraded with 3 cards");
-            }
+        if (amount == 3 && chance > 45) {
+            //Invoke event
+            Debug.Log("Got an upgraded card upgraded with 3 cards");
             // 55 % chance for upgraded rarity
         }
-        if (amount == 4 && !TypeCheck()){
-            if (chance > 5) {
-                //Invoke event
-                Debug.Log("Got an upgraded card upgraded with 4 cards");
-            }
+        if (amount == 4 && chance > 5){
+            //Invoke event
+            Debug.Log("Got an upgraded card upgraded with 4 cards");
             // 95 % chance for upgraded rarity
         }
+        else {
+            return;
+        }
+        
     }
 
     private bool TypeCheck() {
-        if (firstRune.GetComponent<Rune>().runeType == secondRune.GetComponent<Rune>().runeType) {
-            return true;
-        } 
+        Rune mergingRunes = gameObject.GetComponentInChildren<Rune>();
         return false;
-    }
+    } 
 }
